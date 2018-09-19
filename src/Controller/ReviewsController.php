c083532915440162c0891d171e8f1f7e3f457181
bramso brahms <bramsobrahm@gmail.com>
2018-09-19 22:28:35 +0200

    create controller for Order, Reviews and Basket

---
+++ src/Controller/ReviewsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReviewsController extends AbstractController
+{
+    /**
+     * @Route("/reviews", name="reviews")
+     */
+    public function index()
+    {
+        return $this->render('reviews/index.html.twig', [
+            'controller_name' => 'ReviewsController',
+        ]);
+    }
+}
